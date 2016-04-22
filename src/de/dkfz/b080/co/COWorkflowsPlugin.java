398a00467dd393521db6cae8794369d6f115fbed
Michael Heinold <m.heinold@dkfz-heidelberg.de>
2016-04-22 15:33:21 +0200

    Update to 1.1.55

--- src/de/dkfz/b080/co/COWorkflowsPlugin.java
+++ src/de/dkfz/b080/co/COWorkflowsPlugin.java
@@ -8,8 +8,8 @@ import de.dkfz.roddy.plugins.BasePlugin;
  */
 public class COWorkflowsPlugin extends BasePlugin {
 
-    public static final String CURRENT_VERSION_STRING = "1.1.54";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Fri Apr 22 15:20:00 CEST 2016";
+    public static final String CURRENT_VERSION_STRING = "1.1.55";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Fri Apr 22 15:25:53 CEST 2016";
 
     @Override
     public String getVersionInfo() {
