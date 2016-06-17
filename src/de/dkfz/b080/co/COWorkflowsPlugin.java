9f5c0d83bcaa1a5680918fff3202a71dbf4b1743
Philip R. Kensche <kensche@dkfz-heidelberg.de>
2016-06-17 14:58:38 +0200

    Refactoring.

--- src/de/dkfz/b080/co/COWorkflowsPlugin.java
+++ src/de/dkfz/b080/co/COWorkflowsPlugin.java
@@ -8,8 +8,8 @@ import de.dkfz.roddy.plugins.BasePlugin;
  */
 public class COWorkflowsPlugin extends BasePlugin {
 
-    public static final String CURRENT_VERSION_STRING = "1.1.60";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Fri Jun 10 12:42:42 CEST 2016";
+    public static final String CURRENT_VERSION_STRING = "1.1.61";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Thu Jun 16 17:53:06 CEST 2016";
 
     @Override
     public String getVersionInfo() {
