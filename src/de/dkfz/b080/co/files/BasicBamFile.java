4dfbd7fce36e580fc5b363af371f52cecc3c8b3e
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2017-09-25 13:49:06 +0200

    Optimized imports.

--- src/de/dkfz/b080/co/files/BasicBamFile.java
+++ src/de/dkfz/b080/co/files/BasicBamFile.java
@@ -1,14 +1,6 @@
 package de.dkfz.b080.co.files;
 
-import de.dkfz.roddy.core.ExecutionContext;
-import de.dkfz.roddy.execution.jobs.JobDependencyID;
-import de.dkfz.roddy.execution.jobs.JobResult;
 import de.dkfz.roddy.knowledge.files.BaseFile;
-import de.dkfz.roddy.knowledge.files.FileGroup;
-import de.dkfz.roddy.knowledge.files.FileStageSettings;
-
-import java.io.File;
-import java.util.List;
 
 /**
  * Created by heinold on 14.01.16.
